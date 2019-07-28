14c43b206d4b5e0b3f2b29fb074270e6223d725b
kipkemoimayor <collinskipkemoi24@gmail.com>
2019-07-28 10:57:32 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+  'use strict'
+$(document).ready(_=> {
+
+
+const many={
+  kenya:"Naiotoni",
+  uganda:"LAGOs"
+}
+
+let arr=[]
+  arr=many
+
+  console.log(arr)
+})
---
+++ side.js
@@ -0,0 +1,6 @@
+alert("side running")
+export const names={
+  color:"blue",
+  age:10,
+  availability:true
+}
