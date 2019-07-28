5919a8f2be8382247d136b48806540a4c4757df1
kipkemoimayor <collinskipkemoi24@gmail.com>
2019-07-28 21:14:16 +0300

    Added js

--- index.js
+++ index.js
@@ -1,14 +1,47 @@
   'use strict'
 $(document).ready(_=> {
+const addDnDHandlers=()=>
+  {
+    let img=document.querySelector(".img");
+    let zone=document.querySelector("#shoppingcart");
+    let shoppingcart=document.querySelectorAll("#shoppingcart ul")[0];
 
+    for (var i=0;i<img.length;i++){
+      img[i].addEventListener("dragstart",(ev)=>{
+        ev.dataTransfer.effectAllowed='copy';
+        ev.dataTransfer.setData("Text",this.getAttribute("id"));
+      },false)
+    };
 
-const many={
-  kenya:"Naiotoni",
-  uganda:"LAGOs"
-}
+    zone.addEventListener("dragover",function(ev){
+      if (ev.preventDefault)
+          ev.preventDefault();
+      ev.dataTransfer.dropEffect='copy';
+      return false;
+    },false);
 
-let arr=[]
-  arr=many
+    zone.addEventListener("drop",(ev)=>{
+      if (ev.stopPropagation)
+          ev.stopPropagation();
 
-  console.log(arr)
-})
+      var coffeId=ev.dataTransfer.getData("Text");
+      var element=document.getElementById("one1");
+      console.log(coffeId);
+      addCoffeeToShoppingCart(element,coffeId);
+      ev.stopPropagation();
+
+      return false;
+    },false);
+  }
+
+addDnDHandlers()
+
+  function addCoffeeToShoppingCart(item,id) {
+    var html=id+" "+item.getAttribute("data-price");
+
+    var liElement=document.createElement("li");
+    liElement.innerHTML=html;
+    shoppingcart.appendChild(liElement)
+
+  }
+});
